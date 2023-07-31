78938686a914d0de2b92d85931462bb182ba9d36
Alaina Balducci <alainabalducci@gmail.com>
2023-07-31 15:25:05 -0500

    TA working through exercises, finished part 3E

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,23 +1,29 @@
-import './styles.css';
-import Button from './Button';
-import oceans from './oceans.json';
-
+import "./styles.css";
+import Button from "./Button";
+import oceans from "./oceans.json";
 
 function Profile() {
-   let listItem = oceans.map(oceans => 
-      <div key={oceans.id} className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`} >
-         <img src={oceans.image} alt={oceans.name} className='img'/>
-         <h1>{oceans.name}</h1>
-      </div>
-      );
-   return(
-      <>
-         <ul>
-            {listItem}
-         </ul>
-         <Button />
-      </>  
-   );
+  let listItem = oceans.map((oceans) => (
+    <div
+      key={oceans.id}
+      className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}
+    >
+      <img src={oceans.image} alt={oceans.name} className="img" />
+      <h1>{oceans.name}</h1>
+      <h3>Fun Facts:</h3>
+      <ul>
+        <li>{oceans.fact1}</li>
+        <li>{oceans.fact2}</li>
+        <li>{oceans.fact3}</li>
+      </ul>
+    </div>
+  ));
+  return (
+    <>
+      <ul>{listItem}</ul>
+      <Button />
+    </>
+  );
 }
 
 export default Profile;
