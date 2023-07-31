71a228952041abaafe19649ad9d9f5c2e52e9a9a
Alaina Balducci <alainabalducci@gmail.com>
2023-07-31 16:15:59 -0500

    TA working though course material studio part 2- buttons

--- part2/studio/src/components/Button.js
+++ part2/studio/src/components/Button.js
@@ -1,6 +1,17 @@
-function Button() {
-   return;
- }
+import saveButton from "./SaveButton";
+import clickedButton from "./ClickedButton";
+import { render } from "@testing-library/react";
+import SaveButton from "./SaveButton";
+import ClickedButton from "./ClickedButton";
+
+function Button(props) {
+  const saveButton = props.saveButton;
+  if (saveButton){
+    return <SaveButton/>;
+  }
+    return <ClickedButton/>;
+}
+  
  
  export default Button;
  
