70c94f0136f4b23a032b027964c4ee072872ee54
Alaina Balducci <alainabalducci@gmail.com>
2023-07-31 15:11:07 -0500

    TA working through exercises, finished part 3B

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -3,10 +3,17 @@ import Button from './Button';
 import oceans from './oceans.json';
 
 
-function Profile()
-{
+function Profile() {
+   let listItem = oceans.map(ocean => 
+      <div key={ocean.id}>
+         <img src={ocean.image} alt={ocean.name} className='img'/>
+      </div>
+      );
    return(
       <>
+         <ul>
+            {listItem}
+         </ul>
          <Button />
       </>  
    );
